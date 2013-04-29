1f17b26afe58a8a953b52f0c073fc1800a875ba6
Patrick Johnsone <pj0hnson09@yahoo.com>
2013-04-29 00:29:42 -0400

    oh you know how this shit goes.

--- code/js/main.js
+++ code/js/main.js
@@ -1,8 +1,36 @@
 $(document).ready(function(){
+	// $(".btn button").on("click", function(){
+	// 	$(".info").delay(400).fadeOut("slow");
+	// 	$(".message").delay(1000).fadeIn("fast");
+	// })
 
-	$("button").click(function(){
-		$(".info").delay(400).fadeOut("slow");
-		$(".message").delay(1000).fadeIn("fast");
-	})
+	// happy form validation
 
+	$('#signupform').isHappy({
+    fields: {
+      // reference the field you're talking about, probably by `id`
+      // but you could certainly do $('[name=name]') as well.
+      '#name': {
+        required: true,
+        message: 'Please give name'
+      },
+      '#profession': {
+        required: true,
+        message: 'What do you do?'
+      },
+      '#pairprofession': {
+        required: true,
+        message: 'Who do you want to meet?'
+      },
+      '#location': {
+        required: true,
+        message: 'Where do you live?'
+      },
+      '#email': {
+        required: true,
+        message: 'Your email tells us you are'
+      }
+    }
+  });
 });
+
