967c8ecb670b529f5a80d9a1453b3258e8cae592
Patrick Johnsone <pj0hnson09@yahoo.com>
2013-05-03 20:59:18 -0400

    cleaning up

--- code/js/main.js
+++ code/js/main.js
@@ -1,49 +1,10 @@
 $(document).ready(function(){
-
-	$("#form").validate({
-        submitHandler : function(form){
-        	$("#form").delay(500).fadeOut("slow");
-	 		$(".message").delay(1000).fadeIn("slow");
+	 $("#form").validate({
+        submitHandler : function(){
+            $("#form").delay(500).fadeOut("slow", function(){
+                $(".message").fadeIn("slow");
+                return false;	
+            });
         }
-    });
-
-
-
-
-
-    
+    }); 
 });
-	// $("#form").validate();
-	// 	$("#form").submit(function(){
-	// 		$("button").click(function(){
-	// 			$("#form").delay(500).fadeOut("slow");
-	// 			$(".message").delay(1000).fadeIn("slow");
-	// 			return false;
-	// 		})
-			
-	// 	})
-
-	    // $('#form').on("submit", function(){
-	    // 	return false;
-	    //     if ($("#form").valid()){
-	    //         console.log("works");
-	    //         form.submit();
-	    //     } else {
-	    //         console.log("didn't work");
-	    //     }
-
-	    // });
-// s
-
-
-// $("#form").delay(400).fadeOut("slow");
-// $(".message").delay(1000).fadeIn("slow");
-
-		// $("#form").submit(function(){
-
-		// 	return false;
-			
-
-		// $("button").click(function(){
-		// 		$("#form").submit();
-		// });
